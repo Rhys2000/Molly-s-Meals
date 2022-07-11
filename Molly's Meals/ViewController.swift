608cf12c4cd0f17b12cbb46e4c5d609621072060
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-07-11 11:46:15 -0700

    Initial Restaurant struct creation and testing of first text input form

--- Molly's Meals/ViewController.swift
+++ Molly's Meals/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+struct Restaurant {
+    var restaurantName = ""
+}
+
+var listOfRestaurants = [Restaurant]()
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var restaurantNameTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func setDataButton(_ sender: Any) {
+        let name = restaurantNameTextField.text
+        var tempRestaurant = Restaurant()
+        tempRestaurant.restaurantName = name!
+        listOfRestaurants.append(tempRestaurant)
+        restaurantNameTextField.text = ""
+        for restauant in listOfRestaurants {
+            textView.text += restauant.restaurantName
+        }
+    }
+    
 }
 
